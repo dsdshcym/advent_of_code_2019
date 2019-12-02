3ab339773a6793132d41fbafc16d2a28bd9ac45f
Yiming Chen <dsdshcym@gmail.com>
2019-12-02 20:30:46 +0800

    part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -2,4 +2,30 @@ defmodule AoC2019 do
   @moduledoc """
   Documentation for AoC2019.
   """
+
+  def run(stack) do
+    run(stack, 0)
+  end
+
+  defp run(stack, pointer) do
+    op = Enum.at(stack, pointer)
+    pos_1 = Enum.at(stack, pointer + 1)
+    pos_2 = Enum.at(stack, pointer + 2)
+    target = Enum.at(stack, pointer + 3)
+
+    case op do
+      99 ->
+        stack
+
+      1 ->
+        stack
+        |> List.replace_at(target, Enum.at(stack, pos_1) + Enum.at(stack, pos_2))
+        |> run(pointer + 4)
+
+      2 ->
+        stack
+        |> List.replace_at(target, Enum.at(stack, pos_1) * Enum.at(stack, pos_2))
+        |> run(pointer + 4)
+    end
+  end
 end
