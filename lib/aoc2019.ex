5cedf978d0014cdf335afa54cc24a068c746ce5c
Yiming Chen <dsdshcym@gmail.com>
2019-12-05 21:07:13 +0800

    part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -2,4 +2,28 @@ defmodule AoC2019 do
   @moduledoc """
   Documentation for AoC2019.
   """
+
+  def valid_password?(number) do
+    digits = Integer.digits(number)
+
+    six_digits?(digits) &&
+      has_two_adjacent_same_digits?(digits) &&
+      never_decrease?(digits)
+  end
+
+  defp six_digits?(digits) do
+    length(digits) == 6
+  end
+
+  defp has_two_adjacent_same_digits?(digits) do
+    digits
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.any?(fn [a, b] -> a == b end)
+  end
+
+  defp never_decrease?(digits) do
+    digits
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a <= b end)
+  end
 end
--- test/aoc2019_test.exs
+++ test/aoc2019_test.exs
@@ -1,4 +1,28 @@
 defmodule AoC2019Test do
   use ExUnit.Case
   doctest AoC2019
+
+  describe "valid_password?/1" do
+    test "It is a six-digit number" do
+      assert AoC2019.valid_password?(123_356) == true
+      assert AoC2019.valid_password?(123_35) == false
+      assert AoC2019.valid_password?(1_233_567) == false
+    end
+
+    test "Two adjacent digits are the same (like 22 in 122345)" do
+      assert AoC2019.valid_password?(123_367) == true
+      assert AoC2019.valid_password?(234_567) == false
+    end
+
+    test "Going from left to right, the digits never decrease" do
+      assert AoC2019.valid_password?(134_489) == true
+      assert AoC2019.valid_password?(871_345) == false
+    end
+  end
 end
+
+input = 134_564..585_159
+
+input
+|> Enum.count(&AoC2019.valid_password?/1)
+|> IO.inspect(label: "Part 1")
