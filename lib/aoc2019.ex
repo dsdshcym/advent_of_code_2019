ae7fae6cffb24c357157444ea993e7596a6e4679
Yiming Chen <dsdshcym@gmail.com>
2019-12-09 20:47:07 +0800

    refactor: extract Image module and Layer module

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -3,14 +3,43 @@ defmodule AoC2019 do
   Documentation for AoC2019.
   """
 
+  defmodule Image do
+    defstruct [:width, :height, :layers]
+
+    defmodule Layer do
+      defstruct [:width, :height, :pixels]
+
+      def new(pixels, width, height) do
+        %__MODULE__{pixels: pixels, width: width, height: height}
+      end
+
+      def pixels_count(layer, pixel) do
+        layer.pixels
+        |> Enum.count(&Kernel.==(pixel, &1))
+      end
+    end
+
+    def new(pixels, width, height) do
+      layers =
+        pixels
+        |> Enum.chunk_every(width * height)
+        |> Enum.map(&Layer.new(&1, width, height))
+
+      %__MODULE__{layers: layers, width: width, height: height}
+    end
+  end
+
   def p1(input, width, height) do
-    layer =
+    image =
       input
       |> String.codepoints()
       |> Enum.map(&String.to_integer/1)
-      |> Enum.chunk_every(width * height)
-      |> Enum.min_by(&Enum.count(&1, fn pixel -> pixel == 0 end))
+      |> Image.new(width, height)
 
-    Enum.count(layer, &Kernel.==(1, &1)) * Enum.count(layer, &Kernel.==(2, &1))
+    layer =
+      image.layers
+      |> Enum.min_by(&Image.Layer.pixels_count(&1, 0))
+
+    Image.Layer.pixels_count(layer, 1) * Image.Layer.pixels_count(layer, 2)
   end
 end
