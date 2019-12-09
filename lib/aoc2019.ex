4a2cdc163082f1ebfbab13da9dfad0c96d34fcb0
Yiming Chen <dsdshcym@gmail.com>
2019-12-09 20:10:12 +0800

    Part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -2,4 +2,15 @@ defmodule AoC2019 do
   @moduledoc """
   Documentation for AoC2019.
   """
+
+  def p1(input, width, height) do
+    layer =
+      input
+      |> String.codepoints()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(width * height)
+      |> Enum.min_by(&Enum.count(&1, fn pixel -> pixel == 0 end))
+
+    Enum.count(layer, &Kernel.==(1, &1)) * Enum.count(layer, &Kernel.==(2, &1))
+  end
 end
--- test/aoc2019_test.exs
+++ test/aoc2019_test.exs
@@ -1,4 +1,17 @@
 defmodule AoC2019Test do
   use ExUnit.Case
   doctest AoC2019
+
+  @input File.read!(Path.join(__DIR__, "day_8_input.txt")) |> String.trim_trailing()
+
+  describe "p1/1" do
+    test "example" do
+      assert AoC2019.p1("121456789012", 3, 2) == 2
+      assert AoC2019.p1("021456782212", 3, 2) == 3
+    end
+
+    test "test input" do
+      assert AoC2019.p1(@input, 25, 6) == 2032
+    end
+  end
 end
