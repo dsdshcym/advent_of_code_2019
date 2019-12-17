8bcc2d516a82c5ac34ef16a719f4e89bd1fd5669
Yiming Chen <dsdshcym@gmail.com>
2019-12-17 08:42:19 +0800

    Day 12 Part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -2,4 +2,119 @@ defmodule AoC2019 do
   @moduledoc """
   Documentation for AoC2019.
   """
+
+  defmodule Moon do
+    @enforce_keys [:position]
+    defstruct position: nil, velocity: %{x: 0, y: 0, z: 0}
+
+    def new(line) when is_binary(line) do
+      ["x=" <> x_str, "y=" <> y_str, "z=" <> z_str] =
+        String.split(line, ["<", ",", " ", ">"], trim: true)
+
+      new({String.to_integer(x_str), String.to_integer(y_str), String.to_integer(z_str)})
+    end
+
+    def new({x, y, z}) do
+      %__MODULE__{position: %{x: x, y: y, z: z}}
+    end
+
+    def new({x, y, z}, {vx, vy, vz}) do
+      %__MODULE__{
+        position: %{x: x, y: y, z: z},
+        velocity: %{x: vx, y: vy, z: vz}
+      }
+    end
+
+    def apply_gravity(moon, other) do
+      moon
+      |> update_velocity(other, :x)
+      |> update_velocity(other, :y)
+      |> update_velocity(other, :z)
+    end
+
+    defp update_velocity(moon, other, dimension) do
+      %{
+        moon
+        | velocity:
+            Map.update!(
+              moon.velocity,
+              dimension,
+              &(&1 - compare(moon.position[dimension], other.position[dimension]))
+            )
+      }
+    end
+
+    defp compare(d1, d2) when d1 < d2, do: -1
+    defp compare(d1, d2) when d1 == d2, do: 0
+    defp compare(d1, d2) when d1 > d2, do: 1
+
+    def apply_velocity(moon) do
+      moon
+      |> apply_velocity(:x)
+      |> apply_velocity(:y)
+      |> apply_velocity(:z)
+    end
+
+    defp apply_velocity(moon, dimension) do
+      %{
+        moon
+        | position: Map.update!(moon.position, dimension, &(&1 + moon.velocity[dimension]))
+      }
+    end
+
+    def total_energy(moon) do
+      potential_energy(moon) * kinetic_energy(moon)
+    end
+
+    defp potential_energy(moon) do
+      moon.position
+      |> Map.values()
+      |> Enum.map(&abs/1)
+      |> Enum.sum()
+    end
+
+    defp kinetic_energy(moon) do
+      moon.velocity
+      |> Map.values()
+      |> Enum.map(&abs/1)
+      |> Enum.sum()
+    end
+  end
+
+  def p1(input, steps) do
+    input
+    |> parse()
+    |> run(steps)
+    |> Enum.map(&Moon.total_energy/1)
+    |> Enum.sum()
+  end
+
+  def parse(input) do
+    input
+    |> String.trim_trailing()
+    |> String.split("\n")
+    |> Enum.map(&Moon.new/1)
+  end
+
+  def run(moons, 0), do: moons
+
+  def run(moons, step) do
+    moons
+    |> apply_gravities()
+    |> apply_velocities()
+    |> run(step - 1)
+  end
+
+  def apply_gravities(moons) do
+    moons
+    |> Enum.map(fn moon ->
+      moons
+      |> List.delete(moon)
+      |> Enum.reduce(moon, fn other, moon -> Moon.apply_gravity(moon, other) end)
+    end)
+  end
+
+  def apply_velocities(moons) do
+    Enum.map(moons, &Moon.apply_velocity/1)
+  end
 end
--- test/aoc2019_test.exs
+++ test/aoc2019_test.exs
@@ -1,4 +1,74 @@
 defmodule AoC2019Test do
   use ExUnit.Case
   doctest AoC2019
+
+  alias AoC2019.Moon
+
+  @input """
+  <x=-8, y=-18, z=6>
+  <x=-11, y=-14, z=4>
+  <x=8, y=-3, z=-10>
+  <x=-2, y=-16, z=1>
+  """
+
+  test "p1/2" do
+    e1 = """
+    <x=-1, y=0, z=2>
+    <x=2, y=-10, z=-7>
+    <x=4, y=-8, z=8>
+    <x=3, y=5, z=-1>
+    """
+
+    assert AoC2019.p1(e1, 10) == 179
+
+    e2 = """
+    <x=-8, y=-10, z=0>
+    <x=5, y=5, z=10>
+    <x=2, y=-7, z=3>
+    <x=9, y=-8, z=-3>
+    """
+
+    assert AoC2019.p1(e2, 100) == 1940
+
+    assert AoC2019.p1(@input, 1000) == 9743
+  end
+
+  describe "parse/1" do
+    test "example" do
+      assert AoC2019.parse("""
+             <x=-1, y=0, z=2>
+             <x=2, y=-10, z=-7>
+             <x=4, y=-8, z=8>
+             <x=3, y=5, z=-1>
+             """) == [
+               Moon.new({-1, 0, 2}),
+               Moon.new({2, -10, -7}),
+               Moon.new({4, -8, 8}),
+               Moon.new({3, 5, -1})
+             ]
+    end
+  end
+
+  describe "run/2" do
+    test "returns moons when step = 0" do
+      assert AoC2019.run([1, 2, 3, 4], 0) == [1, 2, 3, 4]
+    end
+
+    test "applies gravity and velocity to moons" do
+      assert AoC2019.run(
+               [
+                 Moon.new({-1, 0, 2}),
+                 Moon.new({2, -10, -7}),
+                 Moon.new({4, -8, 8}),
+                 Moon.new({3, 5, -1})
+               ],
+               2
+             ) == [
+               Moon.new({5, -3, -1}, {3, -2, -2}),
+               Moon.new({1, -2, 2}, {-2, 5, 6}),
+               Moon.new({1, -4, -1}, {0, 3, -6}),
+               Moon.new({1, -4, 2}, {-1, -6, 2})
+             ]
+    end
+  end
 end
