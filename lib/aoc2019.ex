2ccbb74213c3a877bc79383e39ec3fb34300f020
Yiming Chen <dsdshcym@gmail.com>
2019-12-17 08:40:04 +0800

    parse input to a menu map

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -2,4 +2,26 @@ defmodule AoC2019 do
   @moduledoc """
   Documentation for AoC2019.
   """
+
+  def to_menu(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
+    |> Enum.into(%{})
+  end
+
+  def parse_line(line) do
+    [materials, product] = String.split(line, " => ")
+
+    materials = materials |> String.split(", ") |> Enum.map(&parse_quantity_chemical/1)
+
+    {product, quantity} = parse_quantity_chemical(product)
+
+    {{product, quantity}, materials}
+  end
+
+  def parse_quantity_chemical(string) do
+    [quantity, chemical] = String.split(string, " ")
+    {chemical, String.to_integer(quantity)}
+  end
 end
--- test/aoc2019_test.exs
+++ test/aoc2019_test.exs
@@ -1,4 +1,24 @@
 defmodule AoC2019Test do
   use ExUnit.Case
   doctest AoC2019
+
+  describe "to_menu/1" do
+    test "example" do
+      input = "10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL"
+
+      assert AoC2019.to_menu(input) == %{
+               {"FUEL", 1} => [{"A", 7}, {"E", 1}],
+               {"E", 1} => [{"A", 7}, {"D", 1}],
+               {"D", 1} => [{"A", 7}, {"C", 1}],
+               {"C", 1} => [{"A", 7}, {"B", 1}],
+               {"B", 1} => [{"ORE", 1}],
+               {"A", 10} => [{"ORE", 10}]
+             }
+    end
+  end
 end
