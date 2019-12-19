335b7c03030fbd61775ee1f612c3ccbdcd19ec20
Yiming Chen <dsdshcym@gmail.com>
2019-12-19 22:47:41 +0800

    Day 19 Part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -3,59 +3,13 @@ defmodule AoC2019 do
   Documentation for AoC2019.
   """
 
-  def part_1(input) do
-    amplifier = IntcodeMachine.new(input)
+  def p1(input) do
+    drone = IntcodeMachine.parse(input)
 
-    for sequence <- permutation(0..4) do
-      sequence
-      |> Enum.reduce(0, fn phase_setting, input ->
-        [output] = amplifier.([phase_setting, input])
-        output
-      end)
+    for x <- 0..49, y <- 0..49 do
+      %{outputs: [output]} = IntcodeMachine.run(%{drone | inputs: [x, y]})
+      output
     end
-    |> Enum.max()
-  end
-
-  def part_2(input) do
-    {:ok, result} =
-      permutation(5..9)
-      |> Task.async_stream(fn sequence ->
-        {:ok, simulator} = AmplifierSimulator.start_link(input, sequence)
-        AmplifierSimulator.result(simulator)
-      end)
-      |> Enum.max_by(fn {:ok, result} -> result end)
-
-    result
-  end
-
-  @doc """
-  iex> AoC2019.permutation([])
-  [[]]
-
-  iex> AoC2019.permutation([1])
-  [[1]]
-
-  iex> AoC2019.permutation(1..3)
-  [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
-  """
-  def permutation([]), do: [[]]
-
-  def permutation(list) when is_list(list) do
-    for h <- list,
-        perm <- permutation(List.delete(list, h)) do
-      [h | perm]
-    end
-  end
-
-  def permutation(enum) do
-    enum
-    |> Enum.to_list()
-    |> permutation()
-  end
-
-  def to_ele_rest([], _, results), do: results
-
-  def to_ele_rest([head | rest], previous, results) do
-    to_ele_rest(rest, [head | previous], [{head, previous ++ rest} | results])
+    |> Enum.sum()
   end
 end
