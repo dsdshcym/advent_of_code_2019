da8c52839452e45c75b20f9338c558eff595aa0c
Yiming Chen <dsdshcym@gmail.com>
2019-12-19 22:55:33 +0800

    calculate patterns first (~2s)

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -4,13 +4,41 @@ defmodule AoC2019 do
   """
 
   def p1(input) do
-    input
-    |> Integer.digits()
-    |> Stream.iterate(&phase/1)
+    digits =
+      input
+      |> Integer.digits()
+
+    phase_fn = phase_fn(length(digits))
+
+    digits
+    |> Stream.iterate(phase_fn)
     |> Enum.at(100)
     |> Enum.take(8)
-    |> Enum.join()
-    |> String.to_integer()
+    |> Integer.undigits()
+  end
+
+  def phase_fn(length) do
+    patterns =
+      for i <- 1..length do
+        [0, 1, 0, -1]
+        |> Stream.map(&List.duplicate(&1, i))
+        |> Stream.concat()
+        |> Stream.cycle()
+        |> Stream.drop(1)
+        |> Enum.take(length)
+      end
+
+    fn digits ->
+      patterns
+      |> Enum.map(fn pattern ->
+        pattern
+        |> Enum.zip(digits)
+        |> Enum.map(fn {value, digit} -> value * digit end)
+        |> Enum.sum()
+        |> abs()
+        |> rem(10)
+      end)
+    end
   end
 
   def phase(digits) do
